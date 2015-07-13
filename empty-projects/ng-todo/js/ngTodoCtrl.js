2296385e76b83363c8d10e06d543f919ddcfa563
Jason <jzhao.690@gmail.com>
2015-07-13 14:54:33 -0700

    add/remove task functions:

--- empty-projects/ng-todo/js/ngTodoCtrl.js
+++ empty-projects/ng-todo/js/ngTodoCtrl.js
@@ -1,12 +1,26 @@
-ngTodo.controller('ngTodoCtrl', function NgTodoCtrl($scope) {
-  $scope.todo = [
-    { val: "A new cool thing", completed: false }
-    { val: "A newwe cool thing", completed: true }
-    { val: "A new cooler thing", completed: false }
-    { val: "A newer cooler thing", completed: false }
-    { val: "A newest cool thing", completed: false }
-    { val: "A new coolest thing", completed: true }
+ngTodo.controller('NgTodoCtrl', function NgTodoCtrl($scope) {
+  $scope.todos = [
+    { val: "A new cool thing", completed: false },
+    { val: "A newer cool thing", completed: true },
+    { val: "A new cooler thing", completed: false },
+    { val: "A newer cooler thing", completed: false },
+    { val: "A newest cool thing", completed: false },
+    { val: "A new coolest thing", completed: true },
   ];
 
-  
+  $scope.addNewTask = function() {
+    $scope.todos.unshift({ completed: false, val: $scope.newTask });
+    $scope.newTask = '';
+  };
+
+  $scope.clearCompleted = function() {
+    $scope.todos = $scope.todos.filter(function(el, index) {
+      return !el.completed;
+    });
+  };
+
+  $scope.removeTodo = function(index) {
+    $scope.todos.splice(index, 1);
+  };
+
 });
